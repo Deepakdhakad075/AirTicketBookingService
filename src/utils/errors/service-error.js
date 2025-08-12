06e088aa5455a05ce7ac7895672a7673c63b89b4
Deepakdhakad075 <ddhakad483@gmail.com>
2025-08-12 17:47:13 +0530

    add error setup

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,13 @@
+const {StatusCodes} = require('http-status-codes')
+class ServiceError extends Error {
+    constructor(
+        message , 
+        explanation,
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    ){ super();
+        this.message = message,
+        this.explanation = explanation,
+        this.statusCode = statusCode
+    }
+}
+module.exports = ServiceError
